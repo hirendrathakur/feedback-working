922db88549e37f2e752eed633eb37537c68b9978
hirendra <hirendrathakur10@gmail.com>
2015-06-14 18:55:16 -0700

    new

--- coursefeedback/coursefeedback/urls.py
+++ coursefeedback/coursefeedback/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     url(r'^$', 'feedback.views.home', name='home'),
     url(r'^feedback/', include('feedback.urls', namespace="feedback")),
